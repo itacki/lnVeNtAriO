13a369aaeafb0874dca2b1e353b92f8f0ea5422a
itacki <jhitacki@gmail.com>
2019-08-24 01:13:01 -0500

    .

--- iNvEnTaRiO/iNvEnTaRiO/Form1.Designer.cs
+++ iNvEnTaRiO/iNvEnTaRiO/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace iNvEnTaRiO
 {
-    partial class Form1
+    partial class Base
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Base
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.IsMdiContainer = true;
+            this.Name = "Base";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- iNvEnTaRiO/iNvEnTaRiO/Form1.cs
+++ iNvEnTaRiO/iNvEnTaRiO/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace iNvEnTaRiO
 {
-    public partial class Form1 : Form
+    public partial class Base : Form
     {
-        public Form1()
+        public Base()
         {
             InitializeComponent();
         }
